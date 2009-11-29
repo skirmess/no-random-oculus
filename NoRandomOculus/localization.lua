d618b881d30a884f3951fb49ca1ffdb036982480
Sven Kirmess <sven.kirmess@kzone.ch>
2009-11-29 16:32:18 +0000

    initial, only English supported yet
    git-svn-id: https://no-random-oculus.googlecode.com/svn/trunk/trunk@8 f928094e-dcfb-11de-95c9-073ad895b44c

---
+++ NoRandomOculus/localization.lua
@@ -0,0 +1,22 @@
+
+-- Copyright (c) 2009, Sven Kirmess
+
+local NoRandomOculus_localization = { }
+
+NoRandomOculus_localization.enUS = {
+	OCULUS					= "The Oculus"
+}
+
+NoRandomOculus_localization.deDE = {
+	OCULUS					= "The Oculus"
+}
+
+NoRandomOculus_localization.frFR = {
+	OCULUS					= "The Oculus"
+}
+
+NoRandomOculus_localization.koKR = {
+}
+
+NoRandomOculus_Constants = NoRandomOculus_localization[GetLocale()]
+
